53b904cff835a3ffc7949f9f06be0afc53257500
cdowdy <cdowdy46@gmail.com>
2013-11-19 14:35:03 -0500

    popup gallery code

---
+++ magnific_popup/blocks/magnific_popup/includes/gallerypopup.php
@@ -0,0 +1,34 @@
+<?php defined('C5_EXECUTE') or die(_("Access Denied."));
+
+
+foreach ($images as $image) {
+	$fileName = $image->getFileName();
+	$picturePath = $image->getRelativePath();
+	$thumbnail = $image->getThumbnail(2, false);
+
+	echo "<a title=\"{$fileName}\" href=\"{$picturePath}\">";
+	echo "<img src=\"{$thumbnail}\" />";
+	echo "</a>";
+}
+?>
+<script>
+$(document).ready(function() {
+$('.popup-gallery').magnificPopup({
+	delegate: 'a',
+	type: 'image',
+	tLoading: 'Loading image #%curr%...',
+	mainClass: 'mfp-img-mobile',
+	gallery: {
+		enabled: true,
+		navigateByImgClick: true,
+		preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+	},
+	image: {
+		tError: '<a href=\"%url%\">The image #%curr%</a> could not be loaded.',
+		titleSrc: function(item) {
+			return item.el.attr('title');
+		}
+	}
+});
+});
+</script>
