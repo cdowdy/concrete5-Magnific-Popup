543da7c2b6b8d1d4e16ffd6e9f81fa344bb94e5f
cdowdy <cdowdy46@gmail.com>
2013-11-20 17:18:22 -0500

    added vimeo and youtube thumbnail options

--- magnific_popup/blocks/magnific_popup/includes/videomapspopup.php
+++ magnific_popup/blocks/magnific_popup/includes/videomapspopup.php
@@ -1,12 +1,21 @@
 <?php defined('C5_EXECUTE') or die(_("Access Denied."));
 	$defaultDelay = 700;
-?>
-<a class="popup-<?php echo $videoOptions; ?>" href="<?php echo $vidMapURL; ?>"><?php echo $vidMapLinkText; ?></a><br>
 
+	$url = $vidMapURL;
+	$str = substr(strrchr($url, '='), 1);
+	$vimStr = substr(strrchr($url, '/'), 1);
+?>
+<a class="popup-<?php echo $videoOptions; ?>" href="<?php echo $vidMapURL; ?>"><?php echo $vidMapLinkText; ?></a><br />
+<?php if ($videoOptions == 'youtubeThumb') :?>
+	<img src="http://img.youtube.com/vi/<?php echo $str. '/'. $youtubeThumbnailOption;?>.jpg" /> <br />
+<?php endif;?>
+<?php if ($videoOptions == 'vimeoThumb') :?>
+	<img src="http://placehold.it/350x150" data-vimeo-id="<?php echo $vimStr;?>" class="<?php echo $vimeoThumbnailOption;?>"/>
+<?php endif; ?>
 
 <script>
 $(document).ready(function() {
-	$('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+	$('.popup-<?php echo $videoOptions;?>').magnificPopup({
 		disableOn: <?php echo $defaultDelay;?>,
 		type: 'iframe',
 		mainClass: 'mfp-fade',
@@ -15,7 +24,6 @@ $(document).ready(function() {
 
 		fixedContentPos: false
 	});
+	$('img').VimeoThumb();
 });
 </script>
-
-
