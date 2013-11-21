9e2b5dc44fecbf413acf8c926cb64f4b7b7a38c9
cdowdy <cdowdy46@gmail.com>
2013-11-21 13:17:17 -0500

    made thumbnails "clickable"

--- magnific_popup/blocks/magnific_popup/includes/videomapspopup.php
+++ magnific_popup/blocks/magnific_popup/includes/videomapspopup.php
@@ -5,12 +5,15 @@
 	$str = substr(strrchr($url, '='), 1);
 	$vimStr = substr(strrchr($url, '/'), 1);
 ?>
-<a class="popup-<?php echo $videoOptions; ?>" href="<?php echo $vidMapURL; ?>"><?php echo $vidMapLinkText; ?></a><br />
 <?php if ($videoOptions == 'youtubeThumb') :?>
-	<img src="http://img.youtube.com/vi/<?php echo $str. '/'. $youtubeThumbnailOption;?>.jpg" /> <br />
+<a class="popup-<?php echo $videoOptions; ?>" href="<?php echo $vidMapURL; ?>"><?php echo $vidMapLinkText; ?>
+	<img src="http://img.youtube.com/vi/<?php echo $str. '/'. $youtubeThumbnailOption;?>.jpg" />
+</a>
 <?php endif;?>
 <?php if ($videoOptions == 'vimeoThumb') :?>
+<a class="popup-<?php echo $videoOptions; ?>" href="<?php echo $vidMapURL; ?>"><?php echo $vidMapLinkText; ?>
 	<img src="http://placehold.it/350x150" data-vimeo-id="<?php echo $vimStr;?>" class="<?php echo $vimeoThumbnailOption;?>"/>
+</a><br />
 <?php endif; ?>
 
 <script>
