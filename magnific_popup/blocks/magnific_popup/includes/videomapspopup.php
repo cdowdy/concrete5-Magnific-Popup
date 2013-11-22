a6defcc37f6e2150f3e33205b6ecbd203acc18c8
cdowdy <cdowdy46@gmail.com>
2013-11-21 20:24:18 -0500

    clickable thumbnail to open lightbox

--- magnific_popup/blocks/magnific_popup/includes/videomapspopup.php
+++ magnific_popup/blocks/magnific_popup/includes/videomapspopup.php
@@ -5,19 +5,18 @@
 	$str = substr(strrchr($url, '='), 1);
 	$vimStr = substr(strrchr($url, '/'), 1);
 ?>
-<div id="<?php echo $bID; ?>">
 	<a class="popup-<?php echo $videoOptions; ?>" href="<?php echo $vidMapURL; ?>"><?php echo $vidMapLinkText; ?>
 <?php if ($videoOptions == 'youtubeThumb') :?>
 		<img src="http://img.youtube.com/vi/<?php echo $str. '/'. $youtubeThumbnailOption;?>.jpg" />
-	</a>
 <?php endif;?>
 <?php if ($videoOptions == 'vimeoThumb') :?>
-		<img src="http://placehold.it/350x150" data-vimeo-id="<?php echo $vimStr;?>" class="<?php echo $vimeoThumbnailOption;?>"/>
-	</a>
+	<img src="http://placehold.it/350x150" data-vimeo-id="<?php echo $vimStr;?>" class="<?php echo $vimeoThumbnailOption;?>"/>
 <?php endif; ?>
+	</a>
 </div>
 <script>
 $(document).ready(function() {
+
 	$('.popup-<?php echo $videoOptions;?>').magnificPopup({
 		disableOn: <?php echo $defaultDelay;?>,
 		type: 'iframe',
