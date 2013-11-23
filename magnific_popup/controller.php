a0dd8ef0d927e6c4e1fdec4d38519b64763c6f4a
cdowdy <cdowdy46@gmail.com>
2013-11-23 13:15:02 -0500

    version bump

--- magnific_popup/controller.php
+++ magnific_popup/controller.php
@@ -4,7 +4,7 @@
 
 		protected $pkgHandle			= 'magnific_popup';
 		protected $appVersionRequired	= '5.6.0';
-		protected $pkgVersion			= '0.0.1';
+		protected $pkgVersion			= '1.0.0';
 
 		public function getPackageName() {
 			return t('Magnific Popup');
