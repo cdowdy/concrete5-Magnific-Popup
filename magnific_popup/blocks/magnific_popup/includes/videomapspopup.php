ed8b52fe0abe2aba1b3a1a0ae883119d15529415
cdowdy <cdowdy46@gmail.com>
2013-11-23 11:37:43 -0500

    cleanup and formatting

--- magnific_popup/blocks/magnific_popup/includes/videomapspopup.php
+++ magnific_popup/blocks/magnific_popup/includes/videomapspopup.php
@@ -7,25 +7,23 @@
 ?>
 	<a class="popup-<?php echo $videoOptions; ?>" href="<?php echo $vidMapURL; ?>"><?php echo $vidMapLinkText; ?>
 <?php if ($videoOptions == 'youtubeThumb') :?>
-		<img src="http://img.youtube.com/vi/<?php echo $str. '/'. $youtubeThumbnailOption;?>.jpg" />
+		<img src="http://img.youtube.com/vi/<?php echo $str. '/'. $youtubeThumbnailOption;?>.jpg" /></a>
 <?php endif;?>
 <?php if ($videoOptions == 'vimeoThumb') :?>
-	<img src="http://placehold.it/350x150" data-vimeo-id="<?php echo $vimStr;?>" class="<?php echo $vimeoThumbnailOption;?>"/>
+	<img src="http://placehold.it/350x150" data-vimeo-id="<?php echo $vimStr;?>" class="<?php echo $vimeoThumbnailOption;?>"/></a>
 <?php endif; ?>
-	</a>
 </div>
 <script>
 $(document).ready(function() {
+$('.popup-<?php echo $videoOptions;?>').magnificPopup({
+	disableOn: <?php echo $defaultDelay;?>,
+	type: 'iframe',
+	mainClass: 'mfp-fade',
+	removalDelay: 160,
+	preloader: false,
 
-	$('.popup-<?php echo $videoOptions;?>').magnificPopup({
-		disableOn: <?php echo $defaultDelay;?>,
-		type: 'iframe',
-		mainClass: 'mfp-fade',
-		removalDelay: 160,
-		preloader: false,
-
-		fixedContentPos: false
-	});
-	$('img').VimeoThumb();
+	fixedContentPos: false
+});
+$('img').VimeoThumb();
 });
 </script>
