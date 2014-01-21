ffa1b1caf9f068646f39756b44fd2d7a02f1d018
cdowdy <cdowdy46@gmail.com>
2014-01-21 15:56:56 -0500

    cleanup and dialog added

--- magnific_popup/blocks/magnific_popup/auto.js
+++ magnific_popup/blocks/magnific_popup/auto.js
@@ -1,28 +1,44 @@
-hideAllTheDivs = function() {
-	$('#single-options, #singleImage, #galleryImages, #youtubeThumb, #vimeoThumb').hide();
+hideAllDivs = function() {
+	$("#single-options").hide();
+	$("#singleImage").hide();
+	$("#galleryImages").hide();
+
+	$("#youtubeThumb").hide();
+	$("#vimeoThumb").hide();
+	$("#animationDialog").hide();
 };
 
-handleSelection = function() {
-	hideAllTheDivs();
+handleNewSelection = function() {
+
+	hideAllDivs();
 
 	switch ($(this).val()) {
-		case 'single': 
-			$('#singleImage').show("slow");
-			$('#single-options').show("slow");
+		case 'single':
+			$("#single-options").show();
+			$("#singleImage").show();
+			$("#vidMap").hide();
 			break;
 		case 'popup':
-			$('#galleryImages').show("slow");
+			$("#galleryImages").show();
+			$("#vidMap").hide();
 			break;
 		case 'zoom':
-			$('#singleImage').show("slow");
+			$("#singleImage").show();
+			$("#vidMap").hide();
 			break;
 		case 'vidMap':
-			$('#vidMap').show("slow");
+			$("#vidMap").show();
 			break;
 		case 'youtubeThumb':
-			$('#youtubeThumb').show("slow");
+			$("#youtubeThumb").show();
+			break;
 		case 'vimeoThumb':
-			$('#vimeoThumb').show("slow");
+			$("#vimeoThumb").show();
+			break;
+		case 'cssdialog':
+			$("#animationDialog").show();
+			$("#vidMap").hide();
+			break;
 	}
 };
 
@@ -32,27 +48,36 @@ function ccmValidateBlockForm() {
 	if ($('#magnific_type').val() == 'select1') {
 		ccm_addError(ccm_t('selection-required'));
 	}
-	if ($('#magnific_type').val() == 'single' && $("#ccm-b-image-fm-value").val() == 0) {
+	if ($('#magnific_type').val() == 'single' && $("#ccm-b-image-fm-value").val() === 0) {
 		ccm_addError(ccm_t('image-required'));
 	}
 	if ($('#magnific_type').val() == 'single' && $('#singleOption').val() == 'select-2') {
 		ccm_addError(ccm_t('single-option-required'));
 	}
-	if ($('#magnific_type').val() == 'popup' && $('#fsID').val() == '') {
+	if ($('#magnific_type').val() == 'popup' && $('#fsID').val() === '') {
 		ccm_addError(ccm_t('gallery-required'));
 	}
-	if ($('#magnific_type').val() == 'zoom' && $("#ccm-b-image-fm-value").val() == 0) {
+	if ($('#magnific_type').val() == 'zoom' && $("#ccm-b-image-fm-value").val() === 0) {
 		ccm_addError(ccm_t('image-required'));
 	}
 	if ($('#magnific_type').val() == 'vidMap' && $('#videoOptions').val() == 'select-3') {
 		ccm_addError(ccm_t('video-map-selection-required'));
 	}
-	if ($('#magnific_type').val() == 'vidMap' && $('#vidMapURL').val() == '') {
+	if ($('#magnific_type').val() == 'vidMap' && $('#vidMapURL').val() === '') {
 		ccm_addError(ccm_t('url-requried'));
 	}
-	if ($('#magnific_type').val() == 'vidMap' && $('#vidMapLinkText').val() == '') {
+	if ($('#magnific_type').val() == 'vidMap' && $('#vidMapLinkText').val() === '') {
 		ccm_addError(ccm_t('link-text-required'));
 	}
-	
+	if ($('#magnific_type').val() == 'cssdialog' && $('#cssDialogLinkText').val() === '') {
+		ccm_addError(ccm_t('dialog-link-text-required'));
+	}
+	if ($('#magnific_type').val() == 'cssdialog' && $('#cssDialogText').val() === '') {
+		ccm_addError(ccm_t('dialog-text-required'));
+	}
+	if ($('#dialogType').val() == 'choose') {
+		ccm_addError(ccm_t('dialog-type-required'));
+	}
+
 	return false;
 }
