67bffadfede8b601d2f081d36909bdb0bde77bdb
cdowdy <cdowdy46@gmail.com>
2014-01-21 15:59:46 -0500

    cleanup

--- magnific_popup/blocks/magnific_popup/includes/videomapspopup.php
+++ magnific_popup/blocks/magnific_popup/includes/videomapspopup.php
@@ -1,27 +1,27 @@
-<?php defined('C5_EXECUTE') or die(_("Access Denied."));
-$v 	  = View::GetInstance(); ?>
+<?php defined( 'C5_EXECUTE' ) or die( _( "Access Denied." ) );
+$v    = View::GetInstance(); ?>
 <div id="<?php echo $magnific_type .'-'.$bID ?>" class="<?php echo $magnific_type. '-gallery'. ' '. $cssFrameworkClass; ?>">
 <?php
-	$defaultDelay = 700;
-	
-	$url          = $vidMapURL;
-	$str          = substr(strrchr($url, '='), 1);
-	$vimStr       = substr(strrchr($url, '/'), 1);
+$defaultDelay = 700;
+
+$url          = $vidMapURL;
+$str          = substr( strrchr( $url, '=' ), 1 );
+$vimStr       = substr( strrchr( $url, '/' ), 1 );
 ?>
 
 	<a class="popup-<?php echo $videoOptions; ?>" href="<?php echo $vidMapURL; ?>"><?php echo $vidMapLinkText; ?>
-<?php if ($videoOptions == 'youtubeThumb') :?>
+<?php if ( $videoOptions == 'youtubeThumb' ) :?>
 		<img class="<?php echo $cssImageClass;?>" src="http://img.youtube.com/vi/<?php echo $str. '/'. $youtubeThumbnailOption;?>.jpg" /></a>
 <?php endif;?>
-<?php if ($videoOptions == 'vimeoThumb') :?>
+<?php if ( $videoOptions == 'vimeoThumb' ) :?>
 	<img class="<?php echo $cssImageClass;?>"  src="http://placehold.it/350x150" data-vimeo-id="<?php echo $vimStr;?>" class="<?php echo $vimeoThumbnailOption;?>"/></a>
 <?php endif; ?>
 </div>
 <?php
 // put these here scripts in the footer
 $page = Page::getCurrentPage();
-if (!$page->isEditMode()) {
-	$v->addFooterItem("<script>
+if ( !$page->isEditMode() ) {
+	$v->addFooterItem( "<script>
 $(document).ready(function() {
 $('.popup-$videoOptions').magnificPopup({
 	disableOn: $defaultDelay,
@@ -34,6 +34,6 @@ $('.popup-$videoOptions').magnificPopup({
 });
 $('img').VimeoThumb();
 });
-</script>");
+</script>" );
 }
 ?>
