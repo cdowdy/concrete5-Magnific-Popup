f2e83a5dbf9cdf28460fad3016b6e6a25340f26d
cdowdy <cdowdy46@gmail.com>
2014-01-21 15:56:15 -0500

    code cleanup

--- magnific_popup/blocks/magnific_popup/add.php
+++ magnific_popup/blocks/magnific_popup/add.php
@@ -1,3 +1,2 @@
-<?php defined('C5_EXECUTE') or die(_("Access Denied."));
-	$this->inc('form_setup_html.php');
-?>
+<?php defined( 'C5_EXECUTE' ) or die( _( "Access Denied." ) );
+$this->inc( 'form_setup_html.php' );
