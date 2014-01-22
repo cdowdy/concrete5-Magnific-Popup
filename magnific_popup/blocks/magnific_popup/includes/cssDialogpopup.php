c19267c83e39652cc5187d17c2789da33b5e65d6
cdowdy <cdowdy46@gmail.com>
2014-01-22 11:54:58 -0500

    add custom animation to view.php

--- magnific_popup/blocks/magnific_popup/includes/cssDialogpopup.php
+++ magnific_popup/blocks/magnific_popup/includes/cssDialogpopup.php
@@ -1,21 +1,30 @@
 <?php defined( 'C5_EXECUTE' ) or die( _( "Access Denied." ) );
-$v    = View::GetInstance();
+$v        = View::GetInstance();
+// variables for the standard/stock animations
+$zoomAnim = 'my-mfp-zoom-in';
+$moveAnim = 'my-mfp-slide-bottom';
+$custom   =  $customAnim;
+
+if ($dialogType == 'zoom-anim') {
+	$mainClass = $zoomAnim;
+} elseif ($dialogType == 'move-anim') {
+	$mainClass = $moveAnim;
+} else {
+	$mainClass = $custom; 
+}
 ?><a class="popup-with-<?php echo $dialogType. '-' . $bID; ?>" href="#small-dialog"><?php echo $cssDialogLinkText; ?></a>
 
 <!-- css dialog popup. mfp-hide makes it hidden -->
 <div id="small-dialog" class="<?php echo $dialogType;?>-dialog mfp-hide">
 	<?php echo $cssDialogText;?>
 </div>
-<?php 
+<?php
 $page = Page::getCurrentPage();
 
 if(!$page->isEditMode()) {
-	switch ($dialogType) {
-		case 'zoom-anim':
-			$v->addFooterItem("<script>
+	$v->addFooterItem("<script>
 $(document).ready(function() {
-	$('.popup-with-zoom-anim-$bID').magnificPopup({
-		
+	$('.popup-with-$dialogType-$bID').magnificPopup({
 		type: 'inline',
 
 		fixedContentPos: false,
@@ -28,32 +37,9 @@ $(document).ready(function() {
 		
 		midClick: true,
 		removalDelay: 300,
-		mainClass: 'my-mfp-zoom-in'
+		mainClass: '$mainClass'
 	});
 });
 </script>");
-			break;
-		case 'move-anim':
-			$v->addFooterItem("<script>
-$(document).ready(function() {
-	$('.popup-with-move-anim-$bID').magnificPopup({
-		type: 'inline',
-
-		fixedContentPos: false,
-		fixedBgPos: true,
-
-		overflowY: 'auto',
-
-		closeBtnInside: true,
-		preloader: false,
-
-		midClick: true,
-		removalDelay: 300,
-		mainClass: 'my-mfp-slide-bottom'
-	});
-});
-</script>");
-			break;
-	}
 }
 ?>
