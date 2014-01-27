d3d9f94915cc54f03dc5d64a771ecbdc6cf6255d
cdowdy <cdowdy46@gmail.com>
2014-01-27 11:05:14 -0500

    added custom animation switch

--- magnific_popup/blocks/magnific_popup/includes/cssDialogpopup.php
+++ magnific_popup/blocks/magnific_popup/includes/cssDialogpopup.php
@@ -1,7 +1,11 @@
 <?php defined('C5_EXECUTE') or die(_( "Access Denied." ));
-$v        = View::GetInstance();
-$page     = Page::getCurrentPage();
+$v      = View::GetInstance();
+$page   = Page::getCurrentPage();
+$custom = $customAnim;
 
+if ($dialogType == 'custom-anim') {
+  $dialogType = $custom;
+}
 ?>
 <div id="dialogPopup-<?php echo $bID; ?>">
   <a href="#dialog-window-<?php echo $bID;?>" data-effect="mfp-<?php echo $dialogType; ?>"><?php echo $cssDialogLinkText; ?></a>
