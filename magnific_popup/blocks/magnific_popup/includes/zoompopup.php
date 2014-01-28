c9c58276231d41cc71bda7ca5fae67d8e520e429
Cory Dowdy <cdowdy46@gmail.com>
2014-01-28 09:25:03 -0500

    Update zoompopup.php

--- magnific_popup/blocks/magnific_popup/includes/zoompopup.php
+++ magnific_popup/blocks/magnific_popup/includes/zoompopup.php
@@ -1,4 +1,4 @@
-<?php defined('C5_EXECUTE') or die(_( "Access Denied." ));
+<?php defined('C5_EXECUTE') or die('Access denied.');
 $page            = Page::getCurrentPage();
 $v               = View::GetInstance();
 
